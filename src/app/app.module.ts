3cb3579ad71c987d29ab13beb487030d0112a150
oscarpon <oscar.pontebaquero@gmail.com>
2021-02-21 20:34:50 +0100

    Con fotos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { IntroComponent } from './intro/intro.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    IntroComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
